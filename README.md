bf5c5aebcd7f063cf6dbb618e83f6c11cecde72f
bruno rodrigues <brnthrbruno@gmail.com>
2021-04-21 16:20:24 -0300

    feat : adicionando as descriç

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# Horas-trabalhadas
-Um aplicativo para medir as horas trabalhadas , simples e composto apenas por JS/CSS/HTML
-Usando para estilização o frameWork boostrap para estilização de alguns componentes
+
+## Worked Hours
+ 
+An application to measure the hours worked.
+## Technology 
+ 
+* JavaScript
+* CSS3
+* HTML
+* Bootstrap
+
+## Services Used
+ 
+* Github
+
+
+## Getting started
+ 
+* To install gems:
+>    $ Git clone git@github.com:bruno-ctrl/Horas-trabalhadas.git
+
+## How to use
+ 
+Abra a pasta do arquivo e execute o index.html
+
+ 
+## Versioning
+ 
+1.0.0.0
+ 
+ 
+## Authors
+ 
+* **Bruno Rodrigues**: @bruno-ctrl (https://github.com/bruno-ctrl/)
+ 
+Please follow github and join us!
+Thanks to visiting me and good coding!
--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function calculate(entry, close, salary) {
 
   const hoursEntry = entry.split(':');
   const hoursClose = close.split(':');
-  const hoursSalary = salary;
 
   let totalHours = hoursClose[0] - hoursEntry[0];
   let totalMinutes = hoursClose[1] - hoursEntry[1];
@@ -28,18 +27,18 @@ function handleText(hours, minutes) {
 
   if (hours || minutes ) {
 
-    const stringTime = `${hours ? hours + ' horas' :''} ${minutes ? minutes + ' minutos' : ''}`;
-    return handleCard("\nhoras trabalhadas : ",stringTime );
+    const stringTime = `${hours ? hours + ' hours' :''} ${minutes ? minutes + ' minutes' : ''}`;
+    return handleCard("\n Worked hours: ",stringTime );
   }
 
-  return handleCard('Error :', 'Valor inválido!')
+  return handleCard('Error :', 'Value invalid!')
 }
 function handleCard(title, message) {
 
   const appendId = document.querySelector('#cardMessage');
   let label = document.createElement('label');
   label.textContent = `${title} ${message}`
-  appendId.innerHTML += label.textContent;
+  appendId.innerHTML = label.textContent;
 }
 
 function start() {
